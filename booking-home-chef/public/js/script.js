cf318bba520081522c1f8a159f21a4dc7032c4f4
Sepideh Attar <sepideh.att@gmail.com>
2022-04-29 18:11:06 +0200

    Project initialization

---
+++ booking-home-chef/public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("booking-home-chef JS imported successfully!");
+  },
+  false
+);
---
+++ booking-home-chef/utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/booking-home-chef";
+
+module.exports = MONGO_URI;
