8fbb21e625898b2358e31e71732eb5b6356f79b7
jidekickpush <jeandenisdrane@gmail.com>
2022-05-02 11:22:50 +0200

    Add list chef to the homepage GET routes

--- booking-home-chef/routes/index.routes.js
+++ booking-home-chef/routes/index.routes.js
@@ -1,15 +1,21 @@
 const isLoggedIn = require("../middleware/isLoggedIn");
 const Recipe = require("../models/Recipe.model");
+const User = require("../models/User.model");
 
 const router = require("express").Router();
 
 
 router.get("/",(req,res,next)=>{
-  
-  Recipe.find()
-  .then(recipesArr=>{
-   res.render("index",{recipes : recipesArr})
-  })
+  let chefs;
+  User.find({isProfilePublic : true})
+    .then(userArr=> {
+      chefs = userArr
+      return Recipe.find()
+    })
+    .then(recipesArr=>{
+      console.log(chefs);
+      res.render("index",{recipes : recipesArr,chefs})
+    })
   .catch(e=>console.log("error to find  list of recipes",e))
 })
 
