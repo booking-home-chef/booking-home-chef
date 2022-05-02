93dcc21a1d23619b2dcac8d441bbb30949c1c13f
jidekickpush <jeandenisdrane@gmail.com>
2022-05-02 14:09:30 +0200

    Update on recipe model to add type of dish and code on Jide's side

--- booking-home-chef/models/Recipe.model.js
+++ booking-home-chef/models/Recipe.model.js
@@ -3,17 +3,24 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const recipeSchema = new Schema(
   {
-   name: String,
-   ingredient:String,
-   description : String,
-   dietary: {
-     type: [String],
-     enum:["Lactose intolerance","Gluten intolerance or sensitivity","Vegetarian","Vegan","Kosher","Keto","Diabetes","Dairy-free","Low carb","Food allergies"]
-   },
-   owner:{
-    type : Schema.Types.ObjectId,
-    ref: "User"
-  }
+    name: String,
+    type: {
+        type: [String],
+        enum: ["starter", "main dish", "dessert", "cocktail piece"],
+        default: [""]
+    },
+    ingredient: String,
+    description: String,
+    dietary: {
+      type: [String],
+      enum: ["Lactose intolerance", "Gluten intolerance or sensitivity", "Vegetarian", "Vegan", "Kosher", "Keto", "Diabetes", "Dairy-free", "Low carb", "Food allergies"],
+      default: "",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      default: ""
+    }
   },
   {
     timestamps: true,
